ac71f2ab9479605a0923f73e239fc4580702596c
hooss-only <hoossonly@gmail.com>
2024-09-20 02:18:55 +0900

    add scene manager

--- src/objects/scene.cpp
+++ src/objects/scene.cpp
@@ -1,8 +1,11 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "scene.hpp"
 
 Scene::Scene() {}
 Scene::~Scene() {}
+
+void Scene::init() {};
 
 void Scene::add_sprite_ticker(SpriteTicker* sprite_ticker) {
 	this->sprite_tickers.push_back(sprite_ticker);
@@ -10,7 +13,7 @@ void Scene::add_sprite_ticker(SpriteTicker* sprite_ticker) {
 
 void Scene::render(sf::RenderWindow* window) {
 	for (auto& sprite_ticker : this->sprite_tickers) {
-		window->draw(*sprite_ticker->sprite);
+		window->draw(*sprite_ticker->sprite); 
 	}
 }
 
@@ -18,4 +21,20 @@ void Scene::tick() {
 	for (auto& sprite_ticker : sprite_tickers) {
 		sprite_ticker->tick();
 	}
+}
+
+SceneManager::SceneManager() {}
+SceneManager::~SceneManager() {}
+
+void SceneManager::render(sf::RenderWindow* window) {
+	std::cout << "rendering scene_ptr: " << current_scene << std::endl;
+	if (this->current_scene == nullptr) {
+		throw std::runtime_error("current_scene is nullptr!");
+	}
+	this->current_scene->render(window);
+}
+
+void SceneManager::set_scene(Scene* scene) {
+	scene->init();
+	this->current_scene = scene;
 }
--- src/objects/scene.hpp
+++ src/objects/scene.hpp
@@ -9,12 +9,25 @@ class Scene {
 		Scene();
 		virtual ~Scene();
 		
+		virtual void init();
 		void add_sprite_ticker(SpriteTicker* sprite);
 		void tick();
 		void render(sf::RenderWindow* window);
 	
 	private:
 		std::vector<SpriteTicker*> sprite_tickers;
+};
+
+class SceneManager {
+	public:
+		SceneManager();
+		~SceneManager();
+
+		void set_scene(Scene* scene);
+		void render(sf::RenderWindow* window);
+
+	private:
+		Scene* current_scene;
 };
 
 #endif
