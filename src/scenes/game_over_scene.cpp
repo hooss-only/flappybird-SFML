4fdd319695b7cbc278b2b22c7cd5afc9a6d2919e
hooss-only <hoossonly@gmail.com>
2024-09-21 19:54:52 +0900

    add gameover text

--- src/scenes/game_over_scene.cpp
+++ src/scenes/game_over_scene.cpp
@@ -1,6 +1,35 @@
+#include "../util/texture.hpp"
+
 #include "game_over_scene.hpp"
 
-GameOverScene::GameOverScene() {}
+class GameOverSpriteTicker : public SpriteTicker {
+	public:
+		GameOverSpriteTicker() {
+			sf::Texture* texture = new sf::Texture();
+			load_texture(texture, "sprites/gameover.png");
+
+			sf::Sprite* sprite = new sf::Sprite();
+			sprite->setTexture(*texture);
+
+			sprite->setScale(sf::Vector2f(2.f, 2.f));
+
+			this->sprite = sprite;
+			this->texture_ptr = texture;
+		}
+
+		~GameOverSpriteTicker() override {}
+};
+
+ButtonSpriteTicker::ButtonSpriteTicker() {}
+ButtonSpriteTicker::~ButtonSpriteTicker() {}
+
+void ButtonSpriteTicker::tick() {};
+
+GameOverScene::GameOverScene() {
+	GameOverSpriteTicker* game_over = new GameOverSpriteTicker();
+	game_over->sprite->setPosition(sf::Vector2f(100, 200));
+	this->add_sprite_ticker(game_over);
+}
 GameOverScene::~GameOverScene() {}
 
 void GameOverScene::event_handler(sf::Event* event) {}
